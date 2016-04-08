119368876d6b7ec3d4a329c78e11b3423b30c689
domi1819 <domi1819@dev.null>
2016-04-08 16:55:21 +0200

    Initial draft

---
+++ UniType/InputForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace domi1819.UniType
+{
+    partial class InputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (this.components != null))
+            {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiUnicodePlusLabel = new System.Windows.Forms.Label();
+            this.uiInputTextBox = new System.Windows.Forms.TextBox();
+            this.uiPreviewLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uiUnicodePlusLabel
+            // 
+            this.uiUnicodePlusLabel.AutoSize = true;
+            this.uiUnicodePlusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiUnicodePlusLabel.Location = new System.Drawing.Point(7, 8);
+            this.uiUnicodePlusLabel.Name = "uiUnicodePlusLabel";
+            this.uiUnicodePlusLabel.Size = new System.Drawing.Size(28, 18);
+            this.uiUnicodePlusLabel.TabIndex = 0;
+            this.uiUnicodePlusLabel.Text = "U+";
+            // 
+            // uiInputTextBox
+            // 
+            this.uiInputTextBox.Location = new System.Drawing.Point(32, 8);
+            this.uiInputTextBox.Name = "uiInputTextBox";
+            this.uiInputTextBox.Size = new System.Drawing.Size(65, 20);
+            this.uiInputTextBox.TabIndex = 1;
+            // 
+            // uiPreviewLabel
+            // 
+            this.uiPreviewLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.uiPreviewLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uiPreviewLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiPreviewLabel.Location = new System.Drawing.Point(106, 2);
+            this.uiPreviewLabel.Name = "uiPreviewLabel";
+            this.uiPreviewLabel.Size = new System.Drawing.Size(49, 32);
+            this.uiPreviewLabel.TabIndex = 0;
+            this.uiPreviewLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(157, 36);
+            this.ControlBox = false;
+            this.Controls.Add(this.uiInputTextBox);
+            this.Controls.Add(this.uiPreviewLabel);
+            this.Controls.Add(this.uiUnicodePlusLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "InputForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label uiUnicodePlusLabel;
+        private System.Windows.Forms.TextBox uiInputTextBox;
+        private System.Windows.Forms.Label uiPreviewLabel;
+
+    }
+}
+
---
+++ UniType/InputForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace domi1819.UniType
+{
+    public partial class InputForm : Form
+    {
+        private bool focusHack;
+
+        public InputForm()
+        {
+            this.InitializeComponent();
+            
+            this.Location = new Point(
+            Screen.PrimaryScreen.WorkingArea.Right - 8 - this.Width, Screen.PrimaryScreen.WorkingArea.Bottom - 8 - this.Height);
+
+            this.uiInputTextBox.TextChanged += this.HandleTextBoxTextChanged;
+            this.uiInputTextBox.LostFocus += this.HandleTextBoxLostFocus;
+            this.uiInputTextBox.KeyPress += this.HandleTextBoxKeyPress;
+
+            RegisterHotKey(this.Handle, 0, 0x0001, (uint) Keys.Add);
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            this.Hide();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312) // WM_HOTKEY
+            {
+                if (!this.Visible)
+                {
+                    this.focusHack = true;
+                    this.Show();
+                    this.Activate();
+                    this.uiInputTextBox.Text = "";
+                    this.focusHack = false;
+                }
+            }
+
+            if (m.Msg != 0x0084) // WM_NCHITTEST
+            {
+                base.WndProc(ref m);
+            }
+        }
+
+        private void HandleTextBoxTextChanged(object sender, EventArgs e)
+        {
+            this.uiPreviewLabel.Text = "";
+
+            try
+            {
+                this.uiPreviewLabel.Text = char.ConvertFromUtf32(int.Parse(this.uiInputTextBox.Text, NumberStyles.HexNumber));
+            }
+            catch (Exception)
+            {
+                // Ignored
+            }
+        }
+
+        private void HandleTextBoxLostFocus(object sender, EventArgs e)
+        {
+            if (!this.focusHack)
+            {
+                this.Hide();
+            }
+        }
+
+        private void HandleTextBoxKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char) Keys.Enter)
+            {
+                this.Hide();
+
+                try
+                {
+                    SendUnicodeText(char.ConvertFromUtf32(int.Parse(this.uiInputTextBox.Text, NumberStyles.HexNumber)));
+                }
+                catch (Exception)
+                {
+                    // Ignored
+                }
+            }
+            else if (e.KeyChar == (char) Keys.Escape)
+            {
+                this.Hide();
+            }
+           
+            if (e.KeyChar != '\b')
+            {
+                if (this.uiInputTextBox.Text.Length >= 6)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+                switch (e.KeyChar)
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                    case 'a':
+                    case 'b':
+                    case 'c':
+                    case 'd':
+                    case 'e':
+                    case 'f':
+                    case 'A':
+                    case 'B':
+                    case 'C':
+                    case 'D':
+                    case 'E':
+                    case 'F':
+                        break;
+                    default:
+                        e.Handled = true;
+                        break;
+                }
+            }
+        }
+
+        private static void SendUnicodeText(string text)
+        {
+            foreach (char utf16Char in text)
+            {
+                INPUT keyInput = new INPUT { Type = 1, ki = { Vk = 0, Scan = utf16Char, Time = 0, Flags = 0x0004, ExtraInfo = 0 } };
+                INPUT[] input = { keyInput, keyInput };
+
+                SendInput(1, input, Marshal.SizeOf(typeof(INPUT)));
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 24)]
+        // ReSharper disable once InconsistentNaming
+        private struct KEYBDINPUT
+        {
+            internal ushort Vk;
+            internal ushort Scan;
+            internal uint Flags;
+            internal uint Time;
+            internal uint ExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        // ReSharper disable once InconsistentNaming
+        private struct INPUT
+        {
+            internal int Type;
+            internal KEYBDINPUT ki;
+        }
+
+        [DllImport("user32", CharSet = CharSet.Unicode)]
+        private static extern uint SendInput(uint numberOfInputs, INPUT[] input, int sizeOfInputStructure);
+
+        [DllImport("user32")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+    }
+}
